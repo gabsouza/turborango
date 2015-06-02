e19fcb561aa1c712815c6f4fc6626e22ecf185ee
Gabriela de Souza <gabrieladessouza@gmail.com>
2015-06-02 11:37:40 -0300

    no message

---
+++ TurboRango.Dominio/TurboRango.Dominio/Categoria.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace TurboRango.Dominio
+{
+    internal enum Categoria
+    {
+        [Description("Comun")]
+        COMUN,
+
+        [Description("Cozinha natural")]
+        COZINHANATURAL,
+
+        [Description("CozinhaMexicana")]
+        COZINHAMEXICANA,
+
+        [Description("CozinhaMexicana")]
+        CHURRASCARIA,
+
+        [Description("CozinhaMexicana")]
+        COZINHAJAPONESA,
+
+        [Description("CozinhaMexicana")]
+        FASTFOOD,
+
+        [Description("CozinhaMexicana")]
+        PIZARRIA,
+    }
+}
+}
---
+++ TurboRango.Dominio/TurboRango.Dominio/Restaurante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    internal class Restaurante
+    {
+        internal string Name { get; set; }
+        internal int Capacidade { get; set; }
+        internal Categoria categoria { get; set; }
+
+    }
+}
