4cc176aa1f73f1d4ed7a487dfd474e12c60053dd
Gabriela de Souza <gabrieladessouza@gmail.com>
2015-06-12 11:48:19 -0300

    no message

--- src/TurboRango/TurboRango.Web/Controllers/ReservasController.cs
+++ src/TurboRango/TurboRango.Web/Controllers/ReservasController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TurboRango.Dominio;
+using TurboRango.Web.Models;
 
 namespace TurboRango.Web.Controllers
 {
     public class ReservasController: Controller
     {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Reservas
         public ActionResult Index()
         {
 
             return View();
+        }
+
+        [AllowAnonymous]
+        public JsonResult Reservas()
+        {
+            //db.Reserva.Add(reserva);
+
+            return Json(new { mensagem = "Reservado!" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
